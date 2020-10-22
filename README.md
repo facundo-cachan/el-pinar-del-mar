c1026795c09544876816f885477d61d3959f6867
Facundo Cachan <fcachan@riu.edu.ar>
2020-10-22 20:42:54 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+<div align="center">
+  <h1>Miyamado Jinja</h1>
+  <img src="https://user-images.githubusercontent.com/7563926/72620461-56b0b200-390d-11ea-99a5-9d05cbcebefb.jpg" height="300">
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/8c59046e-7740-4aa1-a981-a71fbe5822c1/deploy-status)](https://app.netlify.com/sites/miyamadojinja/deploys)
+
+</div>
+
+Miyamado Jinja is a Shinto Shrine located in Yokkaichi, Mie, Japan.
+This project was bootstrapped with [Gatsby](https://www.gatsbyjs.org/).
+
+## 🚀 Quick start
+
+### Prerequisites
+
+1. Git
+1. Node
+1. Yarn
+
+### Installation
+
+Clone this project:
+
+```
+git clone git@github.com:mnishiguchi/miyamadojinja.git
+```
+
+Move to the project directory:
+
+```
+cd miyamadojinja
+```
+
+Install the website's npm dependencies:
+
+```
+yarn
+```
+
+### Running locally
+
+Start the hot-reloading development server (powered by [Gatsby](https://www.gatsbyjs.org)):
+
+```
+yarn start
+```
+
+Open the site in your favorite browser:
+
+```
+open http://localhost:8000
+```
+
+
+## GraphiQL Explorer
+
+This is a tool you can use to experiment with querying your data. Learn more about using this tool in the [Gatsby tutorial](https://www.gatsbyjs.org/tutorial/part-five/#introducing-graphiql).
+
+```
+open http://localhost:8000/___graphql
+````
---
+++ gatsby-config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  siteMetadata: {
+    title: 'El Pinar Del Mar, Aguas Verdes',
+    description:
+      'Alquileres Temporarios de Casas, Cabañas y Departamentos en Costa Atlántica, Buenos Aires. Trato directo con dueños y sin pagar comisiones.',
+    address: 'Avenida 1 73, B7112 Aguas Verdes, Provincia de Buenos Aires',
+    siteUrl: process.env.SITE_URL,
+    gmap:
+      'https://www.google.com/maps/place/El+Pinar+Del+Mar/@-36.6408077,-56.6858634,18z',
+    phone: '54-2257-462146',
+    phoneHref: '542257462146',
+    email: 'consultas@elpinardelmar.com.ar',
+    author: 'Facundo Cachan',
+    facebook: 'https://www.facebook.com/ElPinarDelMar/',
+    instagram: 'https://www.instagram.com/el_pinar_del_mar/',
+  },
+  plugins: [
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'uploads',
+        path: `${__dirname}/static/img`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/img`,
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: process.env.SITE_URL,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `El Pinar Del Mar`,
+        short_name: `El Pinar`,
+        start_url: `/`,
+        background_color: `#000`,
+        theme_color: `#000`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+};
